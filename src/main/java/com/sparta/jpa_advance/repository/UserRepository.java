4920ec1d57c9c3d6684451b8cee495457fc3420d
alscksdlek <mseon916@naver.com>
2025-09-22 14:27:26 +0900

    지연 로딩 & 즉시 로딩

--- src/main/java/com/sparta/jpa_advance/repository/UserRepository.java
+++ src/main/java/com/sparta/jpa_advance/repository/UserRepository.java
@@ -4,4 +4,5 @@ import com.sparta.jpa_advance.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByName(String name);
 }
