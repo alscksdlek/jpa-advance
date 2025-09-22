910845ca805c4eaf6a463cfd6b2bc985592fa56b
alscksdlek <mseon916@naver.com>
2025-09-22 12:44:29 +0900

    @ManyToMany 양방향

--- src/main/java/com/sparta/jpa_advance/entity/User.java
+++ src/main/java/com/sparta/jpa_advance/entity/User.java
@@ -1,8 +1,12 @@
 package com.sparta.jpa_advance.entity;
 
+import com.sparta.jpa_advance.entity.Food;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -13,4 +17,12 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @ManyToMany(mappedBy = "userList")
+    private List<Food> foodList = new ArrayList<>();
+
+    public void addFoodList(Food food) {
+        this.foodList.add(food);
+        food.getUserList().add(this); // 외래 키(연관 관계) 설정
+    }
 }
