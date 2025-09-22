4e49bd6cc8f8663b99eb91204988c93cc86de19f
alscksdlek <mseon916@naver.com>
2025-09-22 11:50:17 +0900

    @ManyToOne 양방향 및 조회

--- src/main/java/com/sparta/jpa_advance/entity/User.java
+++ src/main/java/com/sparta/jpa_advance/entity/User.java
@@ -1,8 +1,12 @@
 package com.sparta.jpa_advance.entity;
 
+import com.sparta.jpa_advance.entity.Food;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -13,4 +17,12 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @OneToMany(mappedBy = "user")
+    private List<Food> foodList = new ArrayList<>();
+
+    public void addFoodList(Food food) {
+        this.foodList.add(food);
+        food.setUser(this); // 외래 키(연관 관계) 설정
+    }
 }
